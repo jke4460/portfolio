0c8319d78b5f68b914d7ad1d085446d624a76bfd
jke4460 <52396663+jke4460@users.noreply.github.com>
2019-08-06 14:35:36 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-    var $menuBtn = $('#menuBtn');
-    var $closeBtn = $('#closeBtn');
-    $menuBtn.on('click', function () {
-        $('#gnb').css({display:'block'});
-    });
-    $closeBtn.on('click', function () {
-        $('#gnb').css({display:'none'});
-    });
-    $menuBtn.on('mouseenter', function () {
-       $(this).addClass('on');
-    });
-    $menuBtn.on('mouseleave', function () {
-       $(this).removeClass('on');
-    });
+	var $menuBtn = $('#menuBtn');
+	var $closeBtn = $('#closeBtn');
+	$menuBtn.on('click', function () {
+			$('#gnb').css({display:'block'});
+	});
+	$closeBtn.on('click', function () {
+			$('#gnb').css({display:'none'});
+	});
+	$menuBtn.on('mouseenter', function () {
+			$(this).addClass('on');
+	});
+	$menuBtn.on('mouseleave', function () {
+			$(this).removeClass('on');
+	});
 
 
-    var $projectsWrapper = $('.wrap');  //프로젝트 소개 그룹 div
+	var $projectsWrapper = $('#content .wrap');  //프로젝트 소개 그룹 div
 	var $projects = $('.project');			//상세 프로젝트 
 	var $lines = $('#indicator .line');		//인디케이터 내부 div
 
@@ -49,6 +49,18 @@ $(document).ready(function () {
 		});
 	});
 
+	$lines.on('click', function (e) {
+		e.preventDefault();
+
+		var tgNum = $(this).data('id') - 1;
+		console.log('인덱스 번호 : ' + tgNum);
+		$projects.eq(tgNum).attr({class: 'project selected'}).siblings().removeClass('selected');
+		$('#indicator ul li').eq(tgNum).children().addClass('active').end().siblings().children().removeClass('active');
+		$('#bg img').removeClass().eq(tgNum).addClass('selected');
+		var moveArr = [0, posArr[0], posArr[1], posArr[2]]; //인디케이터 클릭시 이동위치 배열 추가
+		$projectsWrapper.stop().animate({scrollTop : moveArr[tgNum]});
+	});
+
 	//선택된 상세프로젝트는 project라는 매개변수에, 선택된 인디케이터 라인은 line에 담겨 호출함
 	function selectProjet(project, line) {
 		//활성화 되어 있는 상태라면 강제종료
@@ -58,16 +70,60 @@ $(document).ready(function () {
 		project.attr('class', 'project selected');	//css에서 opacity와 padding을 transition 시키기
 		line.attr('class', 'line active');			//css에서 opacity와 width을 transition 시키기
 		
-		//fixed로 고정된 .background 내부의 이미지는 absolute 속성으로 동일한 data-id를 가지고 있는 이미지를 찾아 .selected를 추가해서 투명도 1과 크기 100%를 가지게 하고 나머지 이미지는 투명도 0과 크기 0.95로 축소 transform: scale(0.95)
-		$('.background img').each(function () {
+		//fixed로 고정된 #bg 내부의 이미지는 absolute 속성으로 동일한 data-id를 가지고 있는 이미지를 찾아 .selected를 추가해서 투명도 1과 크기 100%를 가지게 하고 나머지 이미지는 투명도 0과 크기 0.95로 축소 transform: scale(0.95)
+		$('#bg img').each(function () {
 			if ($(this).data('id') === project.data('id')) $(this).addClass('selected').siblings().removeClass();
 		});
 
 	}
-
 	function unselectProjet(project, line) {
 		project.removeClass('selected'); 
 		line.removeClass('active');
 	}
-    
+
+	slider ('#cnt5');
+	function slider (target) {
+		//console.log(target); #cnt5
+		var $visual = $(target).find('#slider .visual');
+
+		var liWid = $visual.children().outerWidth();
+		//console.log(liWid);  1010
+		var totalNum = $visual.children().length;
+		//console.log(totalNum); 5
+		var current = 1;
+		var visWid = liWid *(totalNum + 2);
+		//console.log(visWid); 7070
+		$visual.css({width : visWid});
+
+		var $front = $visual.children().last().clone().attr({class:'front'});
+		var $back = $visual.children().first().clone().attr({class:'back'});
+		$visual.prepend($front).append($back).css({marginLeft: current * liWid * -1});
+		$visual.children().eq(1).attr({'aria-hidden':false}).siblings().attr({'aria-hidden':true});
+		$('.sliderWrap .control button').on('click', function () {
+			if ($visual.is(':animated')) return false;
+
+			var btnNum = $(this).index();
+			//console.log(btnNum); 0 1
+
+			//이전버튼
+			if (btnNum == 0) {
+				current --;
+				if (current < 1){
+					current = totalNum;
+					$visual.css({marginLeft: (totalNum+1) * -liWid});
+				}
+			}
+			//다음버튼
+			else {
+				current++;
+				if (current > totalNum) {
+					current = 1;
+					$visual.css({marginLeft:0});
+				}
+			}
+			$visual.children().eq(current).attr({'aria-hidden': false}).siblings().attr({'aria-hidden': true});
+			$visual.stop().delay(300).animate({marginLeft:current * -liWid});
+		});
+	}
+
 });
---
+++ js/sub.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var $win = $(window);
+    var winHei = $win.height();
+    function fadeIn () {
+        var tgHei;
+        var tgTop;
+        var start;
+        var end;
+
+        $win.on('scroll', function () {
+            var $fade=$('.fade');
+            var scrollT = $(this).scrollTop();
+            var headHei = 70;
+            //console.log(scrollT);
+
+            $fade.each(function () {
+                tgHei = $(this).outerHeight();
+                tgTop = $(this).offset().top;
+                
+                start = tgTop+tgHei*0.5-winHei;
+                end = tgTop+tgHei *0.5 -headHei;
+
+                if (start < scrollT && end > scrollT) $(this).addClass('on');
+                else $(this).removeClass('on');
+            });
+        });
+    }
+    fadeIn ();
+});
